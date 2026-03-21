cf358daf6d480549ea823ca9a4d66de889c6a7fa
Yohei Oba <gomatty@gmail.com>
2026-03-21 11:28:09 +0900

    20260320-003　github自動ビルド設定

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "20260320-002"
+        versionName = "20260320-003"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
